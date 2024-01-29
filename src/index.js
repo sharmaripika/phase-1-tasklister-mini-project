80549045bcc8702ab79218aff5d4f5b83598cfb4
Ripika Sharma <ripikasharma@gmail.com>
2024-01-29 16:12:33 -0500

    task

--- src/index.js
+++ src/index.js
@@ -1,3 +1,26 @@
 document.addEventListener("DOMContentLoaded", () => {
-  // your code here
+  const form = document.getElementById('create-task-form');
+
+    form.addEventListener('submit', function(event) {
+        event.preventDefault(); // Prevent default form submission behavior
+
+        // Get the value of the task description input
+        const taskDescriptionInput = document.getElementById('new-task-description');
+        const taskDescription = taskDescriptionInput.value;
+
+        // Create a new list item element
+        const listItem = document.createElement('li');
+        listItem.textContent = taskDescription;
+        let btn = document.createElement("button");
+        btn.addEventListener("click", function handleDelete(e){
+          e.target.parentNode.remove()})
+          btn.textContent ="x"
+          listItem.appendChild(btn);
+        // Append the new task to the task list
+        const taskList = document.getElementById('tasks');
+        taskList.appendChild(listItem);
+
+        // Clear the task description input field
+        taskDescriptionInput.value = '';
+    });
 });
